9b36019d6e6c9a662104005c1c9066ef4864453c
Nathan Phan <nathan.hd.phan@gmail.com>
2021-11-10 14:51:17 -0800

    Basic webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/app.js'),
+  },
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'deploy'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'deploy'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Output',
+    }),
+  ],
+};
