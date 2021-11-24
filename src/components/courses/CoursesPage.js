77fdcb94e5e5187a39655730b1aef9da3ec5da83
Nathan Phan <nathan.hd.phan@gmail.com>
2021-11-24 11:17:36 -0800

    Adding redux to application

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,5 +1,61 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as courseActions from '../../redux/actions/courseActions';
+import PropTypes from 'prop-types';
 
-const CoursesPage = () => <h2>Courses</h2>;
+class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: '',
+    },
+  };
 
-export default CoursesPage;
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.actions.createCourse(this.state.course);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+
+        <input type="submit" value="Save" />
+        {this.props.courses.map((course, index) => (
+          <div key={index}>{course.title}</div>
+        ))}
+      </form>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
