a19468973c437053afef5b946f1941c02dbc6746
n-phan <nathan.hd.phan@gmail.com>
2022-01-12 11:38:04 -0800

    Refactoring to use redux-thunk

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,38 +5,20 @@ import * as courseActions from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: '',
-    },
-  };
-
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
+  componentDidMount() {
+    this.props.actions.loadCourses().catch((error) => {
+      alert('Loading courses failed: ' + error);
+    });
+  }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-
-        <input type="submit" value="Save" />
         {this.props.courses.map((course, index) => (
           <div key={index}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
