1a664323b0413598baa29c5c731629b1d72c1823
n-phan <nathan.hd.phan@gmail.com>
2022-01-12 11:07:47 -0800

    Added comment to force prettier re-format

--- src/api/courseApi.js
+++ src/api/courseApi.js
@@ -2,7 +2,9 @@ import { handleResponse, handleError } from './apiUtils';
 const baseUrl = process.env.API_URL + '/courses/';
 
 export function getCourses() {
-  return fetch(baseUrl).then(handleResponse).catch(handleError);
+  return fetch(baseUrl) //
+    .then(handleResponse)
+    .catch(handleError);
 }
 
 export function saveCourse(course) {
