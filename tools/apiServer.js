efc6fc9681b9e4bb6690aab48e1d0815493eea96
n-phan <nathan.hd.phan@gmail.com>
2022-01-12 11:00:01 -0800

    Changed API port from 3001 to 9001

--- tools/apiServer.js
+++ tools/apiServer.js
@@ -58,7 +58,7 @@ server.post('/courses/', function (req, res, next) {
 server.use(router);
 
 // Start server
-const port = 3001;
+const port = 9001;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
